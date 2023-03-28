ed0f4e034b27f1e58dab675c301697855079ca36
Rahul Saini <rsaini@indeed.com>
2023-03-28 17:29:26 +0530

    V1

--- public/script.js
+++ public/script.js
@@ -4,16 +4,18 @@ const gameIntervals = [25, 52];
 const gameAudio = new Audio('media/game.wav');
 const breakAudio = new Audio('media/break.wav');
 
+let interval;
+
 const startTimer = (mins) => {
     const timerElement = document.getElementById('timer');
     let seconds = 60 * mins;
-
-    const x = setInterval(() => {
+    clearInterval(interval);
+    interval = setInterval(() => {
         timerElement.innerText = `${Math.floor(seconds/60)}:${seconds%60}`
         seconds = seconds - 1;
 
         if(seconds <= 0) {
-            clearInterval(x);
+            clearInterval(interval);
             if(breakIntervals.includes(mins)) {
                 breakAudio.play();
             } else if(gameIntervals.includes(mins)) {
